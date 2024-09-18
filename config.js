a9ec258471b896f2357845037fca22c417c16352
queen-nithu <nimsaravihaga37@gmail.com>
2024-09-18 15:48:52 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "",
+ALIVE_MSG : process.env.ALIVE_MSG || "Hello i am Test-MD i am alive now ❤️‍🩹@&",    
+};
