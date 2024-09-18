733e72aca0b55ff0fe58269c9db330342a874a76
queen-nithu <nimsaravihaga37@gmail.com>
2024-09-18 22:56:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '.'
 
 const ownerNumber = ['94761111111']
 
@@ -43,6 +42,11 @@ async function connectToWA() {
 const connectDB = require('./lib/mongodb')
 connectDB();
 //==============================================
+const {readEnv} = require('./lib/database')
+const config = await readEnv();
+const prefix = config.PREFIX
+//==========================
+
 console.log("Connecting NITHU MD BOT 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
