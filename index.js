82112965132b5de3fee145422af5f8f3728af836
queen-nithu <nimsaravihaga37@gmail.com>
2024-09-18 21:23:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, i
 //============================================================================ 
 if(!isOwner && config.MODE === "private") return
 if(!isOwner && isGroup && config.MODE === "inbox") return
-if(!isOwner && !isGroup && config.MODE === "groups") return
+if(!isOwner && isGroup && config.MODE === "groups") return
 //====================react============================
 
 })
