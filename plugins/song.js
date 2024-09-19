763415ea6381668a66c6ae8f2f43accce563698c
queen-nithu <nimsaravihaga37@gmail.com>
2024-09-19 11:23:33 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -4,76 +4,96 @@ const yts = require('yt-search')
 
 cmd({
     pattern: "song",
-    desc: "download songs",
-    category: "download",
-    react: "💿",
+    desc: "downlod song",
+    category: "downlod",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("*suvi tech youtube chanel subcribe කරන්න 🔎...*")
+
+if(!q) return reply("❌Please give me url or titel")
 const search = await yts(q)
-const data = search.videos[0]
-const url = data.url
+const deta = search.videos[0];
+const url = deta.url 
 
-let desc = `*SONG DOWNLOADING.....◆*
- ©NITHU MD
+let desc= `
+*•.¸♡ ASITHA-MD AUDIO-DOWNLOADER🎶 ♡¸.•*
+|__________________________
+| ❤️‍🩹title : ${deta.title}
+| ❤️‍🩹description : ${deta.description}
+| ❤️‍🩹time : ${deta.timestamp}
+| ❤️‍🩹ago : ${deta.ago}
+| ❤️‍🩹views : ${deta.views}
+|__________________________
+
+*POWERED by ASITHA-MD*
+
 `
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
 
-//download audio
+await conn.sendMessage(from,{image :{ url: deta.thumbnail},caption:desc},{quoted:mek});
 
-let down = await fg.yta(url)  
+//downlod audio+ document
+
+let down = await fg.yta(url)
 let downloadUrl = down.dl_url
 
-//send audio
-await conn.sendMessage(from,{audio:{url: downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+//send audio message 
+await conn.sendMessage(from,{audio:{url:downloadUrl},mimetype:"audio/mpeg",caption :"*POWERED by ASITHA-MD*"},{quoted:mek})
+await conn.sendMessage(from,{document:{url:downloadUrl},mimetype:"audio/mpeg",fileName:deta.title + ".mp3" ,caption :"*POWERED by ASITHA-MD*"},{quoted:mek})
+
+  
+
 }catch(e){
+console.log(e)
 reply(`${e}`)
 }
 })
 
-//===========video-dl===========
+//========video dl=======
 
 cmd({
     pattern: "video",
-    desc: "download video",
-    category: "download",
-    react: "🎥",
+    desc: "downlod video",
+    category: "downlod",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("*Suvi tech youtube chanel subcribe කරන්න...*")
+
+if(!q) return reply("❌Please give me url or title")
 const search = await yts(q)
-const data = search.videos[0]
-const url = data.url
+const deta = search.videos[0];
+const url = deta.url 
 
-let des = `*VIDEO DOWNLOADING.....◆*
+let desc= `
+*•.¸♡ ASITHA-MD VIDEO-DOWNLOADER📽️ ♡¸.•*
+|__________________________
+| ❤️‍🩹title : ${deta.title}
+| ❤️‍🩹description : ${deta.description}
+| ❤️‍🩹time : ${deta.timestamp}
+| ❤️‍🩹ago : ${deta.ago}
+| ❤️‍🩹views : ${deta.views}
+|__________________________
 
-| ➤ TITLE - ${data.title}
+*POWERED by ASITHA-MD*
 
-| ➤ VIEWS - ${data.views}
-
-| ➤ DESCRIPTION - ${data.description}
-
-| ➤ TIME - ${data.timestamp}
-
-| ➤ AGO - ${data.ago}
-
-©NITHU MD
 `
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:des},{quoted:mek});
 
-//download video
+await conn.sendMessage(from,{image :{ url: deta.thumbnail},caption:desc},{quoted:mek});
 
-let down = await fg.ytv(url)  
+//downlod video + document 
+
+let down = await fg.ytv(url)
 let downloadUrl = down.dl_url
 
-//send video
-await conn.sendMessage(from,{video:{url: downloadUrl},mimetype:"video/mp4"},{quoted:mek})
-    
-}catch(a){
-reply(`${a}`)
+//send video  message 
+await conn.sendMessage(from,{video:{url:downloadUrl},mimetype:"video/mp4",caption :"*POWERED by ASITHA-MD*"},{quoted:mek})
+await conn.sendMessage(from,{document:{url:downloadUrl},mimetype:"video/mp4",fileName:deta.title + ".mp4",caption :"*POWERED by ASITHA-MD*"},{quoted:mek})
+
+  
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
 }
 })
